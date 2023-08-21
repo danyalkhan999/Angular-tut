3d9d677c6001e405544846075bbe0e0c0e9fc82b
Danyal <danyalkhan8271@gmail.com>
2023-08-21 15:42:22 +0530

    module

--- blog/src/app/app.module.ts
+++ blog/src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { UserAuthModule } from './user-auth/user-auth.module';
 import { AppComponent } from './app.component';
-import { UsersListComponent } from './users-list/users-list.component';
-import { StudentListComponent } from './student-list/student-list.component';
-import { CountryListComponent } from './country-list/country-list.component';
 
 @NgModule({
-    declarations: [AppComponent, UsersListComponent, StudentListComponent, CountryListComponent],
-    imports: [BrowserModule],
+    declarations: [AppComponent],
+    imports: [BrowserModule, UserAuthModule],
     providers: [],
     bootstrap: [AppComponent],
 })
--- blog/src/app/user-auth/login/login.component.spec.ts
+++ blog/src/app/user-auth/login/login.component.spec.ts
@@ -1,16 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { UsersListComponent } from './users-list.component';
+import { LoginComponent } from './login.component';
 
-describe('UsersListComponent', () => {
-  let component: UsersListComponent;
-  let fixture: ComponentFixture<UsersListComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [UsersListComponent]
+      declarations: [LoginComponent]
     });
-    fixture = TestBed.createComponent(UsersListComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
