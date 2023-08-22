247d737ca63e5c4df5dd0c02f88886f787a880a8
danyalkhan999 <danyalkhan8271@gmail.com>
2023-08-22 11:18:34 +0530

    property-binding

--- blog/src/app/app.component.ts
+++ blog/src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Basic Styling rule';
+  title = 'Property Binding in Angular';
+  name= "Danyal";
+  disable= true;
+  functionClick(){
+    if(!this.disable){
+      this.disable=true;
+    }else{
+      this.disable = false;
+    }
+  }
 }
